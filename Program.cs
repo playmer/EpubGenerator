19b925cb41f4b811224f2334d3917855352df986
Joshua T. Fisher <playmer@gmail.com>
2019-07-26 01:50:15 -0700

    Cleanup

--- Program.cs
+++ Program.cs
@@ -7,96 +7,123 @@ namespace EbookHelper
 {
     class Program
     {
-        //<span id='easy-footnote-1-322' class='easy-footnote-margin-adjust'></span><span class='easy-footnote'><a href='#easy-footnote-bottom-1-322' title='A kind of bright pink mollusc.'><sup>1</sup></a></span>
+        ////////////////////////////////////////
+        // Footnotes
+        ////////////////////////////////////////
+        static readonly string cFootnoteContentBegin = "title='";
+        static readonly string cFootnoteContentEnd = "'><sup>";
 
-        static string cFootnoteStart = "title='";
-        static string cFootnoteEnd = "'><sup>";
+        static readonly string cFootnoteBegin = "<span id='easy-footnote";
+        static readonly string cFootnoteEnd = "</sup></a></span>";
 
-        static string parseFootnoteLine(string aLine, int aFootnoteNumber)
+        static string parseLineForFootnotes(string aLine, List<string> aFootnotes)
+        {
+            int start = 0;
+            int end = 0;
+
+            while (aLine.Contains(cFootnoteBegin) && aLine.Contains(cFootnoteEnd))
+            {
+                start = aLine.IndexOf(cFootnoteBegin, 0);
+                end = aLine.IndexOf(cFootnoteEnd, start) + cFootnoteEnd.Length;
+
+                var footnote = aLine.Substring(start, end - start);
+
+                // We haven't added the footnote yet, and they count up from 1.
+                int footnoteNumber = aFootnotes.Count + 1;
+
+                var linkTofootnote = $"<a href=\"index.html#footnote{footnoteNumber}\"><sup>{footnoteNumber}</sup></a>";
+                var footnoteLinkBack = $"<a id=\"backFromFootnote{footnoteNumber}\"></a>";
+
+                aFootnotes.Add(generateFootnoteLine(footnote, footnoteNumber));
+
+                aLine = aLine.Replace(footnote, $"{linkTofootnote}{footnoteLinkBack}");
+            }
+
+            return aLine;
+        }
+
+        static string generateFootnoteLine(string aLine, int aFootnoteNumber)
         {
             var linkFromFootnote = $"<a href=\"index.html#backFromFootnote{aFootnoteNumber}\"><sup>{aFootnoteNumber}</sup></a>";
             var linkToFootnote = $"<a id=\"footnote{aFootnoteNumber}\"></a>";
 
-            int start = aLine.IndexOf(cFootnoteStart, 0) + cFootnoteStart.Length;
-            int end = aLine.IndexOf(cFootnoteEnd, start);
+            int start = aLine.IndexOf(cFootnoteContentBegin, 0) + cFootnoteContentBegin.Length;
+            int end = aLine.IndexOf(cFootnoteContentEnd, start);
 
             var content = aLine.Substring(start, end - start);
 
             return $"{aFootnoteNumber}{linkToFootnote}. {content}{linkFromFootnote}";
         }
 
+        ////////////////////////////////////////
+        // Paragraphs
+        ////////////////////////////////////////
+        /// Parses all lines and places them into the StringBuilder as well as generating a list of footnotes.
+        static void parseParagraphs(List<string> aLines, StringBuilder aBuilder)
+        {
+            var footnotes = new List<string>();
+
+            foreach (var line in aLines)
+            {
+                parseParagraph(line, footnotes, aBuilder);
+            }
+
+            foreach (var footnote in footnotes)
+            {
+                aBuilder.Append("<p class=\"calibre1\">");
+                aBuilder.Append(footnote);
+                aBuilder.Append("</p>");
+                aBuilder.Append("\n");
+            }
+        }
+
+        static void parseParagraph(string aLine, List<string> aFootnotes, StringBuilder aBuilder)
+        {
+            var lineWithoutNewLines = aLine.Replace("\r\n", " ");
+
+            lineWithoutNewLines = parseLineForFootnotes(lineWithoutNewLines, aFootnotes);
+
+
+            aBuilder.Append("<p class=\"calibre1\">");
+            aBuilder.Append(lineWithoutNewLines);
+            aBuilder.Append("</p>");
+            aBuilder.Append("\n");
+        }
+
+        static List<string> parseTextForParagraphs(string aText)
+        {
+            var paragraphs = new List<string>();
+
+            int start = 0;
+            int end = 0;
+
+            string paragraphBegin = "<p>";
+            string paragraphEnd = "</p>";
+
+            while (end < aText.Length)
+            {
+                start = aText.IndexOf("<p>", end) + paragraphBegin.Length;
+                end = aText.IndexOf("</p>", start);
+
+                var line = aText.Substring(start, end - start);
+
+                paragraphs.Add(line);
+
+                end += paragraphEnd.Length;
+            }
+
+            return paragraphs;
+        }
+
+
         static void Main(string[] args)
         {
             var builder = new StringBuilder();
             var text = File.ReadAllText("C:/Users/playm/Documents/ebooks/HaruhiTempoLoss.txt");
 
-            var lines = new List<string>();
+            var paragraphs = parseTextForParagraphs(text);
 
-            {
-                int start = 0;
-                int end = 0;
-
-                string paragraphBegin = "<p>";
-                string paragraphEnd = "</p>";
-
-                while (end < text.Length)
-                {
-                    start = text.IndexOf("<p>", end) + paragraphBegin.Length;
-                    end = text.IndexOf("</p>", start);
-
-                    var line = text.Substring(start, end - start);
-
-                    lines.Add(line);
-
-                    end += paragraphEnd.Length;
-                }
-            }
-
-            var footnotes = new List<string>();
-
-            {
-                var footNoteBegin = "<span id='easy-footnote";
-                var footNoteEnd = "</sup></a></span>";
-
-                foreach (var line in lines)
-                {
-                    var lineWithoutNewLines = line.Replace("\r\n", " ");
-
-                    int start = 0;
-                    int end = 0;
-
-                    while (lineWithoutNewLines.Contains(footNoteBegin) && lineWithoutNewLines.Contains(footNoteEnd))
-                    {
-                        start = lineWithoutNewLines.IndexOf(footNoteBegin, 0);
-                        end = lineWithoutNewLines.IndexOf(footNoteEnd, start) + footNoteEnd.Length;
-
-                        var footnote = lineWithoutNewLines.Substring(start, end - start);
-
-                        // We haven't added the footnote yet, and they count up from 1.
-                        int footnoteNumber = footnotes.Count + 1;
-
-                        var linkTofootnote = $"<a href=\"index.html#footnote{footnoteNumber}\"><sup>{footnoteNumber}</sup></a>";
-                        var footnoteLinkBack = $"<a id=\"backFromFootnote{footnoteNumber}\"></a>";
-
-                        footnotes.Add(parseFootnoteLine(footnote, footnoteNumber));
-
-                        lineWithoutNewLines = lineWithoutNewLines.Replace(footnote, $"{linkTofootnote}{footnoteLinkBack}");
-                    }
-
-                    builder.Append("<p class=\"calibre1\">");
-                    builder.Append(lineWithoutNewLines);
-                    builder.Append("</p>");
-                    builder.Append("\n");
-                }
-            }
-
-            foreach (var footnote in footnotes)
-            {
-                builder.Append("<p class=\"calibre1\">");
-                builder.Append(footnote);
-                builder.Append("</p>");
-                builder.Append("\n");
-            }
+            parseParagraphs(paragraphs, builder);
 
             File.WriteAllText("C:/Users/playm/Documents/ebooks/HaruhiTempoLossProcessed.html", builder.ToString());
         }
